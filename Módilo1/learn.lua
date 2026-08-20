562d3f93db44736a195d62196d2d61fa67e73a28
Sara1502 <sara.moreira1520@gmail.com>
2026-08-20 14:39:44 -0300

    Básicos de Lua

---
+++ Módilo1/learn.lua
@@ -0,0 +1,25 @@
+local MonsterName -- PascalCase
+local MONSTER_NAME  -- SCRIMING_SNAKE_CASE
+local isAngry = true -- camelCase
+
+--- nome do monstro
+local MONSTER_NAME <const> = 'creeper'  
+local health = 20
+local maxHealth = 20
+
+-- comenteario 
+--[[
+comentario de bloco
+]]
+
+print(MONSTER_NAME)
+
+print(type(health))
+
+print(-health)
+
+health = health ^ 2 -- exponenciação
+
+-- quantas vezes eu posso tirar 3 do número health. Devolve o que sobra
+print(health % 3) -- 2
+
