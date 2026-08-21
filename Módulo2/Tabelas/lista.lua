f6514c564afa0549b110ce1df8093a6b4ca8aa09
Sara1502 <sara.moreira1520@gmail.com>
2026-08-21 14:09:08 -0300

    Listas

---
+++ Módulo2/Tabelas/lista.lua
@@ -0,0 +1,21 @@
+
+local names = {'Creeper', 'Zombie', 'Skeleton', 'Phantom'}
+
+
+for i = 1, #names, 1 do
+    print(names[i])
+end
+
+
+print('--------------------')
+
+
+for key,value in pairs(names) do    
+    print(key,value)
+end
+
+print('--------------------')
+
+
+local numbers = {40, 22, 8, 27, 31, 35, 39}
+
