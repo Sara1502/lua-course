fe41981ae62fb8ef326af557ab52d611bbc81c3a
Sara1502 <sara.moreira1520@gmail.com>
2026-08-21 14:42:14 -0300

    Dicionários

---
+++ Módulo2/Tabelas/dict.lua
@@ -0,0 +1,15 @@
+
+local fruits = {
+    Apple = 'Red',
+    Pineapple = 'Yellow',
+    Banana = 'Yellow'
+}
+
+for k,v in pairs(fruits) do
+    print(k,v)
+end
+
+print('------------------------')
+
+local appleColor = fruits['Apple']
+print(string.format( "My apple is %s", appleColor))
