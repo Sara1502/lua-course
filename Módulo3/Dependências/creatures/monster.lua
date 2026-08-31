1b9403feff62d32053b3430352a53147cca23e06
Sara1502 <sara.moreira1520@gmail.com>
2026-08-31 13:19:21 -0300

    Add creature sub-classes animal and monster

---
+++ Módulo3/Dependências/creatures/monster.lua
@@ -0,0 +1,15 @@
+local Creature = require("creatures.creature")
+
+local Monster = Creature:subclass('Monster')
+
+
+-- Constructor
+function Monster:initialize(name)
+    Creature.initialize(self, name)
+end
+
+function Monster:isHostile()
+    return true
+end
+
+return Monster
